1aab7debc292fe14893927ea00032d380e05ac22
fy15912250653 <34825140+fy15912250653@users.noreply.github.com>
2018-01-15 15:33:38 +0800

    修改并规范UserController.java，重新修改UserMapper.xml中insert部分数据库命名

--- src/main/java/com/xcjaas/mediation/controller/UserController.java
+++ src/main/java/com/xcjaas/mediation/controller/UserController.java
@@ -29,54 +29,75 @@ public class UserController {
         return "/html/other-dsr";
     }
 */
-
-    @RequestMapping(value = "/p", method = RequestMethod.GET)
-    public String p() {
+    /*
+        页面跳转1:跳转到personal-info.html，填写个人信息
+     */
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String to_Person_Info_Html() {
         return "/user/personal-info";
     }
 
+    /*
+        页面跳转2:填写完个人信息，跳转至mediate-guide.html，确认条款
+     */
     @RequestMapping(value = "/mediate-guide", method = RequestMethod.GET)
-    public String mg() {
+    public String to_Mediate_Guide_Html() {
         return "/user/mediate-guide";
     }
 
+    /*
+       页面跳转3: 确认条款后，跳转至choice-mediate.html选择调解员
+     */
     @RequestMapping(value = "/choice-mediate", method = RequestMethod.GET)
-    public String cm() {
+    public String to_Choice_Mediate_Html() {
         return "/user/choice-mediate";
     }
 
+    /*
+      页面跳转4: 确认条款后，跳转至other-dsr.html选择其他当事人
+    */
     @RequestMapping(value = "/other-dsr", method = RequestMethod.GET)
-    public String od() {
+    public String to_Other_Dsr_Html() {
         return "/user/other-dsr";
     }
 
-
+    /*
+    在personal-info.html界面，添加用户信息
+     */
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     @ResponseBody
     public int add(@Param("user") User user) {
         userService.addUser(user);
-        if(user.getUserName()==null){
+        if (user.getUserName() == null) {
             //添加失败返回0
             return 0;
-        }else {
+        } else {
             //添加成功返回1
             return 1;
         }
 
     }
-
+    /*
+    跳转至个人主页, personal.html
+     */
     @RequestMapping(value = "/personal", method = RequestMethod.GET)
-    public String up() {
+    public String to_Personal_Html() {
         return "/user/personal";
     }
 
+    /*
+    跳转至我的调解,my-mediate.html
+     */
     @RequestMapping(value = "/my-mediate", method = RequestMethod.GET)
-    public String mm() {
+    public String to_My_Mediate_Html() {
         return "/user/my-mediate";
     }
 
+    /*
+    跳转至评价界面，pingjia.html
+     */
     @RequestMapping(value = "/pingjia", method = RequestMethod.GET)
-    public String pj() {
+    public String to_Pingjia_Html() {
         return "/user/pingjia";
     }
 
